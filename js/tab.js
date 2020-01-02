14ea4e8330d17d87774575f6184f7e19d0e42173
manpreet singh <manpreet.singh10196@gmail.com>
2020-01-02 19:00:57 +0530

    first commit

---
+++ js/tab.js
@@ -0,0 +1,30 @@
+$(function() {
+    var $tabButtonItem = $('#tab-button li'),
+        $tabSelect = $('#tab-select'),
+        $tabContents = $('.tab-contents'),
+        activeClass = 'is-active';
+  
+    $tabButtonItem.first().addClass(activeClass);
+    $tabContents.not(':first').hide();
+  
+    $tabButtonItem.find('a').on('click', function(e) {
+      var target = $(this).attr('href');
+  
+      $tabButtonItem.removeClass(activeClass);
+      $(this).parent().addClass(activeClass);
+      $tabSelect.val(target);
+      $tabContents.hide();
+      $(target).show();
+      e.preventDefault();
+    });
+  
+    $tabSelect.on('change', function() {
+      var target = $(this).val(),
+          targetSelectNum = $(this).prop('selectedIndex');
+  
+      $tabButtonItem.removeClass(activeClass);
+      $tabButtonItem.eq(targetSelectNum).addClass(activeClass);
+      $tabContents.hide();
+      $(target).show();
+    });
+  });
